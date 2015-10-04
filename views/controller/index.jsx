8695f41763f7f86d03c1760700e22c0886ae6d27
TonyQ <tonylovejava@gmal.com>
2015-10-05 07:09:49 +0800

    finetune

--- views/controller/index.jsx
+++ views/controller/index.jsx
@@ -6,7 +6,7 @@ export default class Index extends BaseComponent {
   render(){
     return (
       <div>
-        <h1>預算視覺化清單</h1>
+        <h1>已完成預算視覺化清單</h1>
         
         <ul>
           {this.props.budgets.map((b)=>
